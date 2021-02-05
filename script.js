5a8192b8ef35637536050ebb70943d7e8949e1a8
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-02-05 12:20:55 +0100

    add rules

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//création bouton règles du jeu
+const rules = document.getElementById("rules")
+  rules.addEventListener('click',()=>{
+    alert(`Règles :
+      Le jeu comprend 2 joueurs sur un seul et même écran.
+      Chaque joueur possède un score temporaire (CURRENT) et un score global (SCORE).
+      À chaque tour, le joueur a son CURRENT initialisé à 0 et peut lancer un dé autant de fois qu'il le souhaite. Le
+      résultat d’un lancer est ajouté au CURRENT.
+      Lors de son tour, le joueur peut décider à tout moment de:
+      - Cliquer sur l’option “Réserver”, qui permet d’envoyer les points du CURRENT vers le SCORE. Ce sera alors le
+      tour de l’autre joueur.
+      - Lancer le dé. S’il obtient un 1, son score CURRENT est perdu et c’est la fin de son tour.
+      Le premier joueur qui atteint les 100 points sur SCORE global gagne le jeu.`)
+  })
+
+
 //création nombre aléatoire
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min+1) ) + min;
