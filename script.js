41c333b815c5272f3d337907bb995adc6beb1207
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-02-05 12:17:26 +0100

    add js files with dice number

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-//création bouton règles du jeu
-const rules = document.getElementById("rules")
-  rules.addEventListener('click',()=>{
-    alert(`Règles :
-      Le jeu comprend 2 joueurs sur un seul et même écran.
-      Chaque joueur possède un score temporaire (CURRENT) et un score global (SCORE).
-      À chaque tour, le joueur a son CURRENT initialisé à 0 et peut lancer un dé autant de fois qu'il le souhaite. Le
-      résultat d’un lancer est ajouté au CURRENT.
-      Lors de son tour, le joueur peut décider à tout moment de:
-      - Cliquer sur l’option “Réserver”, qui permet d’envoyer les points du CURRENT vers le SCORE. Ce sera alors le
-      tour de l’autre joueur.
-      - Lancer le dé. S’il obtient un 1, son score CURRENT est perdu et c’est la fin de son tour.
-      Le premier joueur qui atteint les 100 points sur SCORE global gagne le jeu.`)
-  })
-
 //création nombre aléatoire
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min+1) ) + min;
 }
 
-const roll = document.getElementById("roll")
-roll.addEventListener('click',()=>{
-  var numberRandom = getRandomInt(1, 6)
-  alert(`vous avez eu ${numberRandom}`)
-  setTimeout(alert, 3000)
-})
-
-//création du lancé de dé
+const rollDice = () => {document.getElementById("dice").classList.add("rotate-vert-center")};
+const numberRandom = () => {getRandomInt(1, 6)}
+const alertMessageNumberRandom = () => { alert(`vous avez eu ${numberRandom}`)}
 
 
-//recupération du lancé vers current
-const hold = document.getElementById("hold")
-hold.addEventListener('click',()=>{
-  var 
-})
+const roll = document.getElementById("roll");
+roll.addEventListener('click',numberRandom);
+roll.addEventListener('click', alertMessageNumberRandom);
+roll.addEventListener('click', rollDice);
 
-//current score player 1
-const current1 = document.getElementById("currentScore1")
+const currentScore1 = document.getElementById("currentScore1")
+currentScore1 = currentNumber += numberRandom
 
+//creation nb current
+let currentNumber = 0
 
-// Current score player 2
-const current2 = document.getElementById("currentScore2")
-
-
-//création bouton nouvelle partie
-
-
-
-
-
-
-
-
-  
