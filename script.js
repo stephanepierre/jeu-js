17dae30368e8ece2cf62004acb73c75e69dec32b
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-02-06 11:40:55 +0100

    time solution for the dice

--- script.js
+++ script.js
@@ -19,19 +19,14 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min+1) ) + min;
 }
 
-const rollDice = () => {document.getElementById("dice").classList.add("rotate-vert-center")};
-const numberRandom = () => {getRandomInt(1, 6)}
-const alertMessageNumberRandom = () => { alert(`vous avez eu ${numberRandom}`)}
+function rollDice() {
+  let numberRandom = getRandomInt(1, 6);
+  document.getElementById("dice").classList.add("rotate-vert-center");
+  setTimeout(function() {
+    alert(`Vous avez eu ${numberRandom}`);
+    document.getElementById("dice").classList.remove("rotate-vert-center");
+  }, 2500);
+}
 
 
-const roll = document.getElementById("roll");
-roll.addEventListener('click',numberRandom);
-roll.addEventListener('click', alertMessageNumberRandom);
-roll.addEventListener('click', rollDice);
-
-const currentScore1 = document.getElementById("currentScore1")
-currentScore1 = currentNumber += numberRandom
-
-//creation nb current
-let currentNumber = 0
 
