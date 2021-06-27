fac0d171c143b428e762c1fb007002b16fc89473
Pierre <71701076+stephanepierre@users.noreply.github.com>
2021-06-27 13:14:52 +0200

    current score add to reserve scoregit add .

--- script.js
+++ script.js
@@ -14,74 +14,142 @@ const rules = document.getElementById("rules")
   })
 
 
-let score = document.getElementById("score");
-let currentScore = document.getElementById("currentScore");
-
-
-
 
 //............................................................................................
-
 //création of random number
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min+1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// functions for current score 
+let currentScore = 0;
+let reserveScore = 0;
 
-//animation of dice and display of current score
-let oldCurrentScore = document.getElementById("currentScore").textContent = 0;
+function currentScoreLose() {
+  alert("Vous avez perdu")
+  document.getElementById("currentScore").textContent = loseScore;
+  currentScore = 0;
+}
 
 
 function rollDice() {
-  let numberRandom = getRandomInt(1, 6);
+  let randomNumber = getRandomInt(1, 6);
+  if (randomNumber === 1) {
+    currentScoreLose();
+  } else {
+    console.log(randomNumber);
+    currentScore += randomNumber;
+    console.log(currentScore);
+    document.getElementById("currentScore").textContent = currentScore;
+  }
+}
 
-  document.getElementById("dice").classList.add("rotate-vert-center");
-
-  setTimeout(function() {
-    alert(`Vous avez eu ${numberRandom}`);
-    document.getElementById("dice").classList.remove("rotate-vert-center");
-
-    if (numberRandom === 1){
-      document.getElementById("currentScore").textContent = 0;
-      alert(`Perdu, votre score provisoire retombe à 0.`);
-    } else {
-      document.getElementById("currentScore").textContent= oldCurrentScore += numberRandom;
-    }
-
-  }, 2500);
+function reserve() {
+  reserveScore += currentScore;
+  console.log(reserveScore);
+  document.getElementById("reserveScore").textContent = reserveScore;
 }
 
 
-//function to reset current score
-
-function resetCurrentScore() {
-  currentScore.textContent = 0;
-}
-
-//reset score
-function resetScore() {
-  score.textContent = 0;
-}
-
-
-//button new game
-const newGame = document.getElementById("newgame");
-
-newGame.addEventListener("click", () => {
-  resetScore();
-  resetCurrentScore();
-})
-
-//add to score
+//here we call functions
+let roll = document.getElementById("roll");
 let hold = document.getElementById("hold");
-let oldScore = document.getElementById("Score").textContent = 0;
 
-hold.addEventListener("click", () => {
-  score.textContent = oldScore + currentScore.textContent;
-  alert (score.textContent);
-  resetCurrentScore();
+roll.addEventListener("click", rollDice);
+hold.addEventListener("click", reserve);
 
-})
+
+// //création of random number
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min+1) ) + min;
+// }
+
+
+// // functions for current score 
+// let loseScore = 0;
+// let addCurrentScore = 0;
+
+
+// function currentScoreLose(){
+//   alert ("Vous avez perdu")
+//   document.getElementById("currentScore").textContent = loseScore;
+//   addCurrentScore = 0;
+// }
+
+
+// function addCurrentScoreFunction(){
+//   let randomNumber = getRandomInt(1, 6);
+//   if (randomNumber === 1){
+//     currentScoreLose();
+//   } else {
+//     alert (randomNumber)
+//     addCurrentScore += randomNumber;
+//     document.getElementById("currentScore").textContent = addCurrentScore;
+//     let goodScore = document.getElementById("currentScore").textContent;
+//     return goodScore;
+//   }
+// }
+
+
+// //this function add current score to final score when we click on id hold
+// let score = document.getElementById("score");
+
+// function add (){
+//   score.textContent += addCurrentScoreFunction(goodScore);
+// }
+
+
+
+// //here we call functions
+// let roll = document.getElementById("roll");
+// let hold = document.getElementById("hold");
+
+// roll.addEventListener("click", addCurrentScoreFunction);
+// hold.addEventListener("click", add);
+
+
+
+//document.getElementById("hold").addEventListener("click", addCurrentScoreToScore);
+
+
+//while(addScore < 40) {
+  //console.log ("le chiffre alléatoire", randomNumber);
+  //console.log("le nombre fini", addScore);
+  //if (addScore > 40) {
+    //addScore = score;
+    //console.log("vous avez perdu", addScore);
+  //}
+//}
+
+//console.log ("le score initial", score);
+
+
+
+//animation of dice and display of current score
+//let oldCurrentScore = document.getElementById("currentScore").textContent = 0;
+
+
+//function rollDice() {
+  //let numberRandom = getRandomInt(1, 6);
+
+  //document.getElementById("dice").classList.add("rotate-vert-center");
+
+  //setTimeout(function() {
+    //alert(`Vous avez eu ${numberRandom}`);
+    //document.getElementById("dice").classList.remove("rotate-vert-center");
+
+    //if (numberRandom === 1){
+      //document.getElementById("currentScore").textContent = 0;
+      //document.getElementById('currentScore').textContent = 0;
+      //alert(`Perdu, votre score provisoire retombe à 0.`);
+    //} else {
+      //document.getElementById("currentScore").textContent= oldCurrentScore += numberRandom;
+    //}
+  //}, 2500);
+//}
+
+
+
 
 
 
